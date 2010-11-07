828ac54448ff7c32780db6495d76e0e7a1b15199
Gennadiy Shafranovich <fxdemolisher@gmail.com>
2010-11-06 21:06:44 -0400

    preparing for 0.3 release

--- frano/settings.py
+++ frano/settings.py
@@ -5,8 +5,8 @@
 import datetime, os, glob
 
 # build info
-BUILD_VERSION = '0.2'
-BUILD_DATETIME = datetime.datetime(2010, 10, 17, 18, 0, 0)
+BUILD_VERSION = '0.3'
+BUILD_DATETIME = datetime.datetime(2010, 11, 6, 11, 3, 0)
 
 # base db set up, the rest is in environment specific setting files
 DATABASE_ENGINE = 'mysql'
